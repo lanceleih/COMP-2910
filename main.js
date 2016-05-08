0c9dbcd2443961238602bd908ed82ed5311f9499
Lancelei <lancelei.h@hotmail.ca>
2016-05-08 12:21:10 -0700

    created icons folder for the icon images

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
 /* Javascript for All Parts of Game */ 
 
 /* Game Paused Page Script */
-
-<script>
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
-ctx.font = "30px Arial";
-ctx.fillText("GAME PAUSED",10,50);
-</script>
