2c9c87253f69e01048c656c5e087035a19cf5bd5
Haley Booker <haley1652@hotmail.com>
2016-05-10 12:16:51 -0700

    used the new buttons

--- main.js
+++ main.js
@@ -14,11 +14,11 @@ function home() {
     var img=document.getElementById("slogan");
     ctx.drawImage(img, 60, 420, 200, 50);
     var img=document.getElementById("firstButton");
-    ctx.drawImage(img,30,150, 300, 150);
+    ctx.drawImage(img,90,180, 150, 75);
     var img=document.getElementById("secondButton");
-    ctx.drawImage(img,30,230, 300, 150);
+    ctx.drawImage(img,90,260, 150, 75);
     var img=document.getElementById("thirdButton");
-    ctx.drawImage(img,30,310, 300, 150);
+    ctx.drawImage(img,90,340, 150, 75);
     
     home.addEventListener("click", getPosition, false);
 
@@ -33,58 +33,13 @@ function home() {
         y -= home.offsetTop;
         //alert(x + ',' + y);
    
-        if(x > 70 && x < 250 && y > 190 && y < 250) {
+        if(x > 90 && x < 240 && y > 180 && y < 255) {
             ctx.clearRect(0, 0, 320, 480);
             selectShape();
-        } else if(x > 70 && x < 250 && y > 270 && y < 330) {
+        } else if(x > 90 && x < 240 && y > 260 && y < 335) {
             //ctx.clearRect(0, 0, 320, 480);
-        } else if(x > 70 && x < 250 && y > 350 && y < 410) {
+        } else if(x > 90 && x < 240 && y > 340 && y < 415) {
             //ctx.clearRect(0, 0, 320, 480);
-        }
-    } 
-}
-function selectShape() {
-    var home = document.getElementById("mainCanvas");
-    var ctx = home.getContext("2d");
-    document.getElementById("title").src = "title/shapetitle.png";
-    document.getElementById("firstButton").src = "icons/rectangle.png";
-    document.getElementById("secondButton").src = "icons/polygon.png";
-    document.getElementById("thirdButton").src = "icons/hexa.png";
-    var img=document.getElementById("title");
-    ctx.drawImage(img, 15, 40, 300, 132);
-    var img=document.getElementById("firstButton");
-    ctx.drawImage(img,10,180, 200, 200);
-    var img=document.getElementById("secondButton");
-    ctx.drawImage(img,150,180, 200, 200);
-    var img=document.getElementById("thirdButton");
-    ctx.drawImage(img,10,280, 200, 200);
-    var img=document.getElementById("backButton")
-    ctx.drawImage(img, 0, 0, 125, 50);
-    
-    home.addEventListener("click", getPosition, false);
-
-    function getPosition(event)
-    {
-        var x = event.x;
-        var y = event.y;
-        var home = document.getElementById("mainCanvas");
-        var ctx = home.getContext("2d");
-
-        x -= home.offsetLeft;
-        y -= home.offsetTop;
-        //alert(x + ',' + y);
-        if(x > 40 && x < 150 && y > 230 && y < 320) {
-            ctx.clearRect(0, 0, 320, 480);
-            selectDifficulty();
-        } else if(x > 180 && x < 300 && y > 230 && y < 320) {
-            ctx.clearRect(0, 0, 320, 480);
-            selectDifficulty();
-        } else if(x > 40 && x < 150 && y > 330 && y < 410) {
-            ctx.clearRect(0, 0, 350, 480);
-            selectDifficulty();
-        } else if(x > 20 && x < 100 && y > 20 && y < 40) {
-            ctx.clearRect(0, 0, 320, 480);
-            backHome();
         }
     } 
 }
@@ -101,11 +56,11 @@ function backHome() {
     var img=document.getElementById("slogan");
     ctx.drawImage(img, 60, 420, 200, 50);
     var img=document.getElementById("firstButton");
-    ctx.drawImage(img,30,150, 300, 150);
+    ctx.drawImage(img,90,180, 150, 75);
     var img=document.getElementById("secondButton");
-    ctx.drawImage(img,30,230, 300, 150);
+    ctx.drawImage(img,90,260, 150, 75);
     var img=document.getElementById("thirdButton");
-    ctx.drawImage(img,30,310, 300, 150);
+    ctx.drawImage(img,90,340, 150, 75);
     
     home.addEventListener("click", getPosition, false);
 
@@ -120,13 +75,58 @@ function backHome() {
         y -= home.offsetTop;
         //alert(x + ',' + y);
    
-        if(x > 70 && x < 250 && y > 190 && y < 250) {
+        if(x > 90 && x < 240 && y > 180 && y < 255) {
             ctx.clearRect(0, 0, 320, 480);
             selectShape();
-        } else if(x > 70 && x < 250 && y > 270 && y < 330) {
+        } else if(x > 90 && x < 240 && y > 260 && y < 335) {
             //ctx.clearRect(0, 0, 320, 480);
-        } else if(x > 70 && x < 250 && y > 350 && y < 410) {
+        } else if(x > 90 && x < 240 && y > 340 && y < 415) {
             //ctx.clearRect(0, 0, 320, 480);
+        }
+    } 
+}
+function selectShape() {
+    var home = document.getElementById("mainCanvas");
+    var ctx = home.getContext("2d");
+    document.getElementById("title").src = "title/shapetitle.png";
+    document.getElementById("firstButton").src = "icons/square.png";
+    document.getElementById("secondButton").src = "icons/polygon.png";
+    document.getElementById("thirdButton").src = "icons/hexa.png";
+    var img=document.getElementById("title");
+    ctx.drawImage(img, 15, 40, 300, 132);
+    var img=document.getElementById("firstButton");
+    ctx.drawImage(img,20,180, 125, 100);
+    var img=document.getElementById("secondButton");
+    ctx.drawImage(img,180,180, 125, 100);
+    var img=document.getElementById("thirdButton");
+    ctx.drawImage(img,20,300, 125, 100);
+    var img=document.getElementById("backButton")
+    ctx.drawImage(img, 0, 0, 100, 50);
+    
+    home.addEventListener("click", getPosition, false);
+
+    function getPosition(event)
+    {
+        var x = event.x;
+        var y = event.y;
+        var home = document.getElementById("mainCanvas");
+        var ctx = home.getContext("2d");
+
+        x -= home.offsetLeft;
+        y -= home.offsetTop;
+        //alert(x + ',' + y);
+        if(x > 20 && x < 145 && y > 180 && y < 280) {
+            ctx.clearRect(0, 0, 320, 480);
+            selectDifficulty();
+        } else if(x > 180 && x < 305 && y > 180 && y < 280) {
+            ctx.clearRect(0, 0, 320, 480);
+            selectDifficulty();
+        } else if(x > 20 && x < 145 && y > 300 && y < 400) {
+            ctx.clearRect(0, 0, 350, 480);
+            selectDifficulty();
+        } else if(x > 20 && x < 100 && y > 20 && y < 40) {
+            ctx.clearRect(0, 0, 320, 480);
+            backHome();
         }
     } 
 }
@@ -140,13 +140,13 @@ function selectDifficulty() {
     var img=document.getElementById("title");
     ctx.drawImage(img, 15, 40, 300, 132);
     var img=document.getElementById("firstButton");
-    ctx.drawImage(img,30,150, 300, 150);
+    ctx.drawImage(img,90,180, 150, 75);
     var img=document.getElementById("secondButton");
-    ctx.drawImage(img,30,230, 300, 150);
+    ctx.drawImage(img,90,260, 150, 75);
     var img=document.getElementById("thirdButton");
-    ctx.drawImage(img,30,310, 300, 150);
+    ctx.drawImage(img,90,340, 150, 75);
     var img=document.getElementById("backButton")
-    ctx.drawImage(img, 0, 0, 125, 50);
+    ctx.drawImage(img, 0, 0, 100, 50);
 
     home.addEventListener("click", getPosition, false);
 
@@ -161,11 +161,12 @@ function selectDifficulty() {
         y -= home.offsetTop;
         //alert(x + ',' + y);
    
-        if(x > 70 && x < 250 && y > 190 && y < 250) {
+        if(x > 90 && x < 240 && y > 180 && y < 255) {
+            ctx.clearRect(0, 0, 320, 480);
+            selectShape();
+        } else if(x > 90 && x < 240 && y > 260 && y < 335) {
             //ctx.clearRect(0, 0, 320, 480);
-        } else if(x > 70 && x < 250 && y > 270 && y < 330) {
-            //ctx.clearRect(0, 0, 320, 480);
-        } else if(x > 70 && x < 250 && y > 350 && y < 410) {
+        } else if(x > 90 && x < 240 && y > 340 && y < 415) {
             //ctx.clearRect(0, 0, 320, 480);
         } else if(x > 20 && x < 100 && y > 20 && y < 40) {
             ctx.clearRect(0, 0, 320, 480);
