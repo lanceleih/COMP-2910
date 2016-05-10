742a718968c229038e53d842cff46a6a23678142
Haley Booker <haley1652@hotmail.com>
2016-05-10 10:50:28 -0700

    added select difficulty page

--- main.js
+++ main.js
@@ -74,11 +74,14 @@ function selectShape() {
         y -= home.offsetTop;
         //alert(x + ',' + y);
         if(x > 40 && x < 150 && y > 230 && y < 320) {
-            //ctx.clearRect(0, 0, 320, 480);
+            ctx.clearRect(0, 0, 320, 480);
+            selectDifficulty();
         } else if(x > 180 && x < 300 && y > 230 && y < 320) {
-            //ctx.clearRect(0, 0, 320, 480);
+            ctx.clearRect(0, 0, 320, 480);
+            selectDifficulty();
         } else if(x > 40 && x < 150 && y > 330 && y < 410) {
-            //ctx.clearRect(0, 0, 350, 480);
+            ctx.clearRect(0, 0, 350, 480);
+            selectDifficulty();
         } else if(x > 20 && x < 100 && y > 20 && y < 40) {
             ctx.clearRect(0, 0, 320, 480);
             backHome();
@@ -127,3 +130,47 @@ function backHome() {
         }
     } 
 }
+function selectDifficulty() {
+    var home = document.getElementById("mainCanvas");
+    var ctx = home.getContext("2d");
+    document.getElementById("firstButton").src = "icons/easy.png";
+    document.getElementById("secondButton").src = "icons/medium.png";
+    document.getElementById("thirdButton").src = "icons/hard.png";
+    document.getElementById("title").src = "title/difficultytitle.png";
+    var img=document.getElementById("title");
+    ctx.drawImage(img, 15, 40, 300, 132);
+    var img=document.getElementById("firstButton");
+    ctx.drawImage(img,30,150, 300, 150);
+    var img=document.getElementById("secondButton");
+    ctx.drawImage(img,30,230, 300, 150);
+    var img=document.getElementById("thirdButton");
+    ctx.drawImage(img,30,310, 300, 150);
+    var img=document.getElementById("backButton")
+    ctx.drawImage(img, 0, 0, 125, 50);
+
+    home.addEventListener("click", getPosition, false);
+
+    function getPosition(event)
+    {
+        var x = event.x;
+        var y = event.y;
+        var home = document.getElementById("mainCanvas");
+        var ctx = home.getContext("2d");
+
+        x -= home.offsetLeft;
+        y -= home.offsetTop;
+        //alert(x + ',' + y);
+   
+        if(x > 70 && x < 250 && y > 190 && y < 250) {
+            //ctx.clearRect(0, 0, 320, 480);
+        } else if(x > 70 && x < 250 && y > 270 && y < 330) {
+            //ctx.clearRect(0, 0, 320, 480);
+        } else if(x > 70 && x < 250 && y > 350 && y < 410) {
+            //ctx.clearRect(0, 0, 320, 480);
+        } else if(x > 20 && x < 100 && y > 20 && y < 40) {
+            ctx.clearRect(0, 0, 320, 480);
+            selectShape();
+        }
+    } 
+}
+
