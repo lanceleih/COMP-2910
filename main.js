7b1bb4575bc15d9039ddbefdb74633860524efcb
Haley Booker <haley1652@hotmail.com>
2016-05-10 09:58:41 -0700

    Added image titles to main and select shape. added background colour

--- main.js
+++ main.js
@@ -1,30 +1,24 @@
 /* Javascript for All Parts of Game */ 
 
-/* Game Paused Page Script */
-
-
-var canvas = document.getElementById("mainCanvas");
-var ctx = canvas.getContext("2d");
-ctx.font = "30px Arial";
-ctx.fillText("GAME PAUSED",10,50);
-
-
+/* Javascript for the home page*/
 function home() {
     var home = document.getElementById("mainCanvas");
     var ctx = home.getContext("2d");
-    ctx.font = "30px Arial";
-    ctx.fillText("Scholours",90,80);
-    ctx.font = "20px Arial";
-    ctx.fillText('The "U" makes it Canadian',40,420);
     document.getElementById("firstButton").src = "icons/play.png";
     document.getElementById("secondButton").src = "icons/settings.png";
     document.getElementById("thirdButton").src = "icons/leaderboard.png";
+    document.getElementById("title").src = "title/titleIcon.png";
+    document.getElementById("slogan").src= "title/slogan.png";
+    var img=document.getElementById("title");
+    ctx.drawImage(img, 15, 40, 300, 132);
+    var img=document.getElementById("slogan");
+    ctx.drawImage(img, 60, 420, 200, 50);
     var img=document.getElementById("firstButton");
-    ctx.drawImage(img,30,80, 300, 150);
+    ctx.drawImage(img,30,150, 300, 150);
     var img=document.getElementById("secondButton");
-    ctx.drawImage(img,30,170, 300, 150);
+    ctx.drawImage(img,30,230, 300, 150);
     var img=document.getElementById("thirdButton");
-    ctx.drawImage(img,30,260, 300, 150);
+    ctx.drawImage(img,30,310, 300, 150);
     
     home.addEventListener("click", getPosition, false);
 
@@ -37,32 +31,33 @@ function home() {
 
         x -= home.offsetLeft;
         y -= home.offsetTop;
+        //alert(x + ',' + y);
    
-        if(x > 30 && x < 330 && y > 110 && y < 180) {
+        if(x > 70 && x < 250 && y > 190 && y < 250) {
             ctx.clearRect(0, 0, 320, 480);
             selectShape();
-            //alert(x + ',' + y);
-        } else if(x > 30 && x < 330 && y > 200 && y < 270) {
-            ctx.clearRect(0, 0, 320, 480);
-        } else if(x > 30 && x < 330 && y > 290 && y < 360) {
-            ctx.clearRect(0, 0, 320, 480);
+        } else if(x > 70 && x < 250 && y > 270 && y < 330) {
+            //ctx.clearRect(0, 0, 320, 480);
+        } else if(x > 70 && x < 250 && y > 350 && y < 410) {
+            //ctx.clearRect(0, 0, 320, 480);
         }
     } 
 }
 function selectShape() {
     var home = document.getElementById("mainCanvas");
     var ctx = home.getContext("2d");
-    ctx.font = "30px Arial";
-    ctx.fillText("Select Shape",70,90);
+    document.getElementById("title").src = "title/shapetitle.png";
     document.getElementById("firstButton").src = "icons/rectangle.png";
     document.getElementById("secondButton").src = "icons/polygon.png";
     document.getElementById("thirdButton").src = "icons/hexa.png";
+    var img=document.getElementById("title");
+    ctx.drawImage(img, 15, 40, 300, 132);
     var img=document.getElementById("firstButton");
-    ctx.drawImage(img,30,50, 300, 250);
+    ctx.drawImage(img,10,180, 200, 200);
     var img=document.getElementById("secondButton");
-    ctx.drawImage(img,30,170, 300, 250);
+    ctx.drawImage(img,150,180, 200, 200);
     var img=document.getElementById("thirdButton");
-    ctx.drawImage(img,30,290, 300, 250);
+    ctx.drawImage(img,10,280, 200, 200);
     var img=document.getElementById("backButton")
     ctx.drawImage(img, 0, 0, 125, 50);
     
@@ -78,13 +73,13 @@ function selectShape() {
         x -= home.offsetLeft;
         y -= home.offsetTop;
         //alert(x + ',' + y);
-        if(x > 70 && x < 250 && y > 110 && y < 210) {
-            ctx.clearRect(0, 0, 320, 480);
-        } else if(x > 70 && x < 250 && y > 230 && y < 330) {
-            ctx.clearRect(0, 0, 320, 480);
-        } else if(x > 70 && x < 250 && y > 330 && y < 450) {
-            ctx.clearRect(0, 0, 350, 480);
-        } else if(x > 0 && x < 100 && y > 0 && y < 50) {
+        if(x > 40 && x < 150 && y > 230 && y < 320) {
+            //ctx.clearRect(0, 0, 320, 480);
+        } else if(x > 180 && x < 300 && y > 230 && y < 320) {
+            //ctx.clearRect(0, 0, 320, 480);
+        } else if(x > 40 && x < 150 && y > 330 && y < 410) {
+            //ctx.clearRect(0, 0, 350, 480);
+        } else if(x > 20 && x < 100 && y > 20 && y < 40) {
             ctx.clearRect(0, 0, 320, 480);
             backHome();
         }
@@ -93,19 +88,20 @@ function selectShape() {
 function backHome() {
     var home = document.getElementById("mainCanvas");
     var ctx = home.getContext("2d");
-    ctx.font = "30px Arial";
-    ctx.fillText("Scholours",90,80);
     ctx.font = "20px Arial";
-    ctx.fillText('The "U" makes it Canadian',40,420);
+    ctx.fillText('The "U" makes it Canadian',40,460);
     document.getElementById("firstButton").src = "icons/play.png";
     document.getElementById("secondButton").src = "icons/settings.png";
     document.getElementById("thirdButton").src = "icons/leaderboard.png";
+    document.getElementById("title").src = "title/titleIcon.png";
+    var img=document.getElementById("title");
+    ctx.drawImage(img, 15, 40, 300, 132);
     var img=document.getElementById("firstButton");
-    ctx.drawImage(img,30,80, 300, 150);
+    ctx.drawImage(img,30,150, 300, 150);
     var img=document.getElementById("secondButton");
-    ctx.drawImage(img,30,170, 300, 150);
+    ctx.drawImage(img,30,230, 300, 150);
     var img=document.getElementById("thirdButton");
-    ctx.drawImage(img,30,260, 300, 150);
+    ctx.drawImage(img,30,310, 300, 150);
     
     home.addEventListener("click", getPosition, false);
 
@@ -118,15 +114,15 @@ function backHome() {
 
         x -= home.offsetLeft;
         y -= home.offsetTop;
+        //alert(x + ',' + y);
    
-        if(x > 30 && x < 330 && y > 110 && y < 180) {
+        if(x > 70 && x < 250 && y > 190 && y < 250) {
             ctx.clearRect(0, 0, 320, 480);
             selectShape();
-            //alert(x + ',' + y);
-        } else if(x > 30 && x < 330 && y > 200 && y < 270) {
-            ctx.clearRect(0, 0, 320, 480);
-        } else if(x > 30 && x < 330 && y > 290 && y < 360) {
-            ctx.clearRect(0, 0, 320, 480);
+        } else if(x > 70 && x < 250 && y > 270 && y < 330) {
+            //ctx.clearRect(0, 0, 320, 480);
+        } else if(x > 70 && x < 250 && y > 350 && y < 410) {
+            //ctx.clearRect(0, 0, 320, 480);
         }
     } 
 }
