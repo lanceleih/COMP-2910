da467bc6770cbd5d8a2a1d438e4fcedc3cc045c5
Lancelei <lancelei.h@hotmail.ca>
2016-05-19 14:31:40 -0700

    PHP for leaderboard square

--- php/leaderboardSquare.php
+++ php/leaderboardSquare.php
@@ -7,6 +7,10 @@
     mysql_connect($servername,$username,$password);
     mysql_select_db('a7385043_2910') or die( "Unable to select database");
 
+    SELECT * 
+    FROM  `leaderboard` 
+    LIMIT 0 , 200
+
 
 
 ?>
