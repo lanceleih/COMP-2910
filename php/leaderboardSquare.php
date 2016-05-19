8bcf25c44741cdfc45b3558bf1691bb2bcfb0f2f
Haley Booker <haley1652@hotmail.com>
2016-05-19 14:46:28 -0700

    trying to select rows

--- php/leaderboardSquare.php
+++ php/leaderboardSquare.php
@@ -4,13 +4,22 @@
     $password = "Scholours16";
     $db = "a7385043_2910";
 
-    mysql_connect($servername,$username,$password);
+    $conn = mysql_connect($servername,$username,$password);
     mysql_select_db('a7385043_2910') or die( "Unable to select database");
 
-    SELECT * 
-    FROM  `leaderboard` 
-    LIMIT 0 , 200
+    $sql = "SELECT id, name, time, shape, difficulty FROM leaderboard"; 
+    $result = mysqli_query($conn, $sql);
 
+    if (mysqli_num_rows($result) > 0) {
+        // output data of each row
+        while($row = mysqli_fetch_assoc($result)) {
+            echo "id: " . $row["id"]. " - Name: " . $row["name"]. " - Time: " . $row["time"]."<br>";
+        }
+    } else {
+        echo "0 results";
+    }
 
+    mysqli_close($conn);
+    name, id, time, shape, difficulty
 
 ?>
