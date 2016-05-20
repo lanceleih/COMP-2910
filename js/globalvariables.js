ba5dc2409d97526cfea67f30036505e3ff0e243b
Gigolo Dan <vvdshin@gmail.com>
2016-05-19 18:56:01 -0700

    global changes

--- js/globalvariables.js
+++ js/globalvariables.js
@@ -1,20 +1,20 @@
 /* Javascript for All Parts of Game */
 /*Global variables*/
-    var canvas;
-    var ctx;
+var canvas;
+var ctx;
 
-    /* Audio: BGM & SFX */
-    var bgm = new Audio("../audio/shopExtended.mp3");
-    var sfx1 = new Audio("../audio/tap.wav");
-    var sfx2 = new Audio("../audio/click.wav");
+/* Audio: BGM & SFX */
+var bgm = new Audio("../audio/shopExtended.mp3");
+var sfx1 = new Audio("../audio/tap.wav");
+var sfx2 = new Audio("../audio/click.wav");
 
 
-    /*0=square 1=diamond 2=hexagon*/
-    var shape;
-    /*0=easy 1=medium 2=hard*/
-    var difficulty;
+/*0=square 1=diamond 2=hexagon*/
+var shape;
+/*0=easy 1=medium 2=hard*/
+var difficulty;
 
-    /*Colour Inventory*/
+/*Colour Inventory*/
 // Jungle Love
 var palette0 = ["#998365", "#56FF6F", "#FF80DC", "#FEF216"];
 // Autumn's Splendor
@@ -75,58 +75,57 @@ var danCount = 0;
 var palette = palette4;
 
 
-
 // new global variables
 // offsets, margins, and x, y, width, height of every components in game
-    var offset_left;
-    var offset_right;
-    var offset_top;
-    var offset_bottom;
-    var margin_x;
-    var margin_y;
-    var timer_x;
-    var timer_y;
-    var timer_width;
-    var timer_height;
-    var pause_x;
-    var pause_y;
-    var pause_width;
-    var pause_height;
-    var inventory_width;
-    var inventory_height;
-    var board_x;
-    var board_y;
-    var board_width;
-    var board_height;
-    var inventory_x;
-    var inventory_y;
+var offset_left;
+var offset_right;
+var offset_top;
+var offset_bottom;
+var margin_x;
+var margin_y;
+var timer_x;
+var timer_y;
+var timer_width;
+var timer_height;
+var pause_x;
+var pause_y;
+var pause_width;
+var pause_height;
+var inventory_width;
+var inventory_height;
+var board_x;
+var board_y;
+var board_width;
+var board_height;
+var inventory_x;
+var inventory_y;
 
 // variable values that change based on shape and difficulty
-    var max_row;
-    var max_col;
-    var max_tile;
-    var tile_width;
-    var tile_height;
+var max_row;
+var max_col;
+var max_tile;
+var tile_width;
+var tile_height;
 
 //specific units for hexagon -- unit width
-    var uw;
+var uw;
 //specific units for hexagon -- unit height
-    var uh;
+var uh;
 
 // tile array
-    var tiles;
+var tiles;
 // coloring palette array
 //var palette = ["#A1C4A6", "#FBD78D", "#F5634A", "#953B32"];
 // default color -- white
-    var default_color = "#FFFFFF";
+var default_color = "#FFFFFF";
 
 // timer
-    var gameTimer;
+var gameTimer;
 // the starting time
-    var startTime;
+var startTime;
 // how long has it been since timer started
-    var elapsedTime;
+var elapsedTime;
 // color inventory array
-    var colorInventory = new Array(4);
+var colorInventory = new Array(4);
 // how many tiles has been colored -- fires validation when all tiles are colored
-    var tilesColored;
+var tilesColored;
