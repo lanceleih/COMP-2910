42d9587a264da8484157c4e737c21a461a32363b
Haley Booker <haley1652@hotmail.com>
2016-05-26 10:40:18 -0700

    linked tutorial and renamed images

--- js/tutorial.js
+++ js/tutorial.js
@@ -12,14 +12,10 @@ function firstTutorial() {
     ctx.drawImage(firstImage, 50, 60, 220, 400);
     var img = document.getElementById("backButton");
     ctx.drawImage(img, 0, 0, 100, 50);
-    /*var secondimg = document.getElementById("2");
-     ctx.drawImage(secondimg, 80, 70, 200, 70);
-     var thirdimg = document.getElementById("slogan");
-     ctx.drawImage(thirdimg, 60, 420, 200, 50);
-     var fourthimg = document.getElementById("playButton");
-     ctx.drawImage(fourthimg, 90, 180, 150, 75);*/
     canvas.addEventListener("click", getPosition, false);
-
+    ctx.strokeStyle = '#000';  // some color/style
+    ctx.lineWidth = 5;         // thickness
+    ctx.strokeRect( 50, 60, 220, 400);
     function getPosition() {
         var x = event.x;
         var y = event.y;
@@ -29,7 +25,90 @@ function firstTutorial() {
             canvas.removeEventListener("click", getPosition, false);
             sfx1.play();
             selectShape();
+        } else if(x > 278 * widthFactor + leftMargin && x < 318 * widthFactor + leftMargin && y > 280 * heightFactor + topMargin && y < 326 * heightFactor + topMargin) {
+            canvas.removeEventListener("click", getPosition, false);
+            sfx1.play();
+            secondTutorial();
+        } else if (x > 5 * widthFactor + leftMargin && x < 45 * widthFactor + leftMargin && y > 280 * heightFactor + topMargin && y < 326 * heightFactor + topMargin) {
+            canvas.removeEventListener("click", getPosition, false);
+            sfx1.play();
+            thirdTutorial();
+        }
+    }
+}
+function secondTutorial() {
+    ctx.clearRect(0, 0, 320, 480);
+    var leftArrowTutorial = document.getElementById("leftArrow");
+    var rightArrowTutorial = document.getElementById("rightArrow");
 
+    //Left arrow
+    ctx.drawImage(leftArrowTutorial, 5, 280, 40, 46);
+    //Right arrow
+    ctx.drawImage(rightArrowTutorial, 278, 280, 40, 46);
+
+    var secondImage = document.getElementById("2");
+    ctx.drawImage(secondImage, 50, 60, 220, 400);
+    var img = document.getElementById("backButton");
+    ctx.drawImage(img, 0, 0, 100, 50);
+    canvas.addEventListener("click", getPosition, false);
+    ctx.strokeStyle = '#000';  // some color/style
+    ctx.lineWidth = 5;         // thickness
+    ctx.strokeRect( 50, 60, 220, 400);
+    function getPosition() {
+        var x = event.x;
+        var y = event.y;
+
+        if (x > 15 * widthFactor + leftMargin && x < 100 * widthFactor + leftMargin && y > 15 * heightFactor + topMargin && y < 40 * heightFactor + topMargin) {
+            // back to home
+            canvas.removeEventListener("click", getPosition, false);
+            sfx1.play();
+            selectShape();
+        } else if (x > 278 * widthFactor + leftMargin && x < 318 * widthFactor + leftMargin && y > 280 * heightFactor + topMargin && y < 326 * heightFactor + topMargin) {
+            canvas.removeEventListener("click", getPosition, false);
+            sfx1.play();
+            thirdTutorial();
+        } else if (x > 5 * widthFactor + leftMargin && x < 45 * widthFactor + leftMargin && y > 280 * heightFactor + topMargin && y < 326 * heightFactor + topMargin) {
+            canvas.removeEventListener("click", getPosition, false);
+            sfx1.play();
+            firstTutorial();
+        }
+    }
+}
+function thirdTutorial() {
+    ctx.clearRect(0, 0, 320, 480);
+    var leftArrowTutorial = document.getElementById("leftArrow");
+    var rightArrowTutorial = document.getElementById("rightArrow");
+
+    //Left arrow
+    ctx.drawImage(leftArrowTutorial, 5, 280, 40, 46);
+    //Right arrow
+    ctx.drawImage(rightArrowTutorial, 278, 280, 40, 46);
+
+    var secondImage = document.getElementById("3");
+    ctx.drawImage(secondImage, 50, 60, 220, 400);
+    var img = document.getElementById("backButton");
+    ctx.drawImage(img, 0, 0, 100, 50);
+    canvas.addEventListener("click", getPosition, false);
+    ctx.strokeStyle = '#000';  // some color/style
+    ctx.lineWidth = 5;         // thickness
+    ctx.strokeRect( 50, 60, 220, 400);
+    function getPosition() {
+        var x = event.x;
+        var y = event.y;
+
+        if (x > 15 * widthFactor + leftMargin && x < 100 * widthFactor + leftMargin && y > 15 * heightFactor + topMargin && y < 40 * heightFactor + topMargin) {
+            // back to home
+            canvas.removeEventListener("click", getPosition, false);
+            sfx1.play();
+            selectShape();
+        } else if (x > 278 * widthFactor + leftMargin && x < 318 * widthFactor + leftMargin && y > 280 * heightFactor + topMargin && y < 326 * heightFactor + topMargin) {
+            canvas.removeEventListener("click", getPosition, false);
+            sfx1.play();
+            firstTutorial();
+        } else if (x > 5 * widthFactor + leftMargin && x < 45 * widthFactor + leftMargin && y > 280 * heightFactor + topMargin && y < 326 * heightFactor + topMargin) {
+            canvas.removeEventListener("click", getPosition, false);
+            sfx1.play();
+            secondTutorial();
         }
     }
 }
