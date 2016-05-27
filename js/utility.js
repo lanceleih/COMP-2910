f33e0d0d48f92762c238d1bb0d3f6c3fe1024acc
Lancelei <lancelei.h@hotmail.ca>
2016-05-27 02:17:22 -0700

    little tweak

--- js/utility.js
+++ js/utility.js
@@ -57,9 +57,9 @@ function setResponMargins() {
     }
 
     canvasWidthUserInput = canvasWidth - (canvasWidth * .90);
-    leftMarginUserInput = ((docWidth - (canvasWidth * 0.15)) / 2);
+    leftMarginUserInput = ((docWidth - (canvasWidth * 0.10)) / 2);
     canvasWidthSubmitUserButton = canvasWidth - (canvasWidth * 0.80);
-    leftMarginSubmitUserButton = ((docWidth - (canvasWidth * -0.15)) / 2);
+    leftMarginSubmitUserButton = ((docWidth - (canvasWidth * -0.2)) / 2);
 
     mainDiv.style.marginLeft =  leftMargin.toString() + "px";
     userInputName.style.marginLeft =  leftMarginUserInput.toString() + "px";
